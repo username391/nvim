6c84a0e2bcd19b432bcfd2dd8c3cdaa8f3d86304
username391 <kilrea@yandex.ru>
2023-09-21 23:53:41 +0500

    useless space deleted

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -52,7 +52,7 @@ vim.keymap.set("n", "<Space><Space>", builtin.oldfiles,   {})
 vim.keymap.set("n", "<Space>fg",      builtin.live_grep,  {})
 vim.keymap.set("n", "<Space>fh",      builtin.help_tags,  {})
 
-vim.api.nvim_set_keymap("n", "U", ":Telescope buffers<CR>", { noremap = true, silent = true} )
+vim.api.nvim_set_keymap("n", "U", ":Telescope buffers<CR>", { noremap = true, silent = true })
 
 -- BarBar
 vim.keymap.set("n", "<A-,>", "<Cmd>BufferPrevious<CR>", { noremap = true, silent = true})
