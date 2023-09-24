ba2df8bcd6b07279ed6193dcf0680d1c1f08cf5d
username391 <kilrea@yandex.ru>
2023-09-24 10:07:27 +0500

    рефактор

--- lua/plugins/lightspeed.lua
+++ lua/plugins/lightspeed.lua
@@ -1,11 +1,14 @@
-return {
-	"ggandor/lightspeed.nvim",
-
-	config = function ()
-		require("lightspeed").setup{
-			ignore_case = false,
-			exit_after_idle_msecs = { unlabeled = nil, labeled = nil },
-		}
-	end
+local M = {
+	"ggandor/lightspeed.nvim"
 }
+
+M.enabled = false -- Пока выключу, вроде не пользуюсь
+M.config = function ()
+	require("lightspeed").setup{
+		ignore_case = false,
+		exit_after_idle_msecs = { unlabeled = nil, labeled = nil },
+	}
+end
+
+return M
 
