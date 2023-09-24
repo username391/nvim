1d80f3d597845cca08bff42955aa11d498d9a8ee
username391 <kilrea@yandex.ru>
2023-09-24 20:20:01 +0500

    Убрал прозрачность инпутов

--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -5,6 +5,11 @@ local M = {
 M.enabled = true
 M.config = function()
 	require("dressing").setup {
+		input = {
+			win_options = {
+				winblend = 0
+			}
+		},
 		select = {
 			telescope = {
 				previewer = false,
