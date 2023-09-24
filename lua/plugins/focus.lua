b58707fdc8e06543845e601cd5a513729abc3ce9
username391 <kilrea@yandex.ru>
2023-09-24 09:50:54 +0500

    Добавил расширение для автоизменения размера сплита

---
+++ lua/plugins/focus.lua
@@ -0,0 +1,40 @@
+local M = {
+	"nvim-focus/focus.nvim"
+}
+
+M.enabled = true
+M.version = false
+
+M.config = function ()
+	require("focus").setup({
+		autoresize = {
+			enable = true,
+			width = 0,
+			height = 0,
+			minwidth = 0,
+			minheight = 0,
+			height_quickfix = 10,
+		},
+		split = {
+			bufnew = false,
+			tmux = false,
+		},
+		ui = {
+			number = false,
+			relativenumber = false,
+			hybridnumber = false,
+			absolutenumber_unfocussed = true,
+
+			cursorline = true,
+			cursorcolumn = false,
+			colorcolumn = {
+				enable = false,
+				list = '+1',
+			},
+			signcolumn = true,
+			winhighlight = true,
+		}
+	})
+end
+
+return M
