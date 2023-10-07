63235af668ad2d5033720d18dec766b55a71d1fd
= <me@box>
2023-10-08 00:40:11 +0500

    update of ibl

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -20,20 +20,18 @@ M.dependencies = {
 	{
 		"lukas-reineke/indent-blankline.nvim",
 		main = "ibl",
-		config = function()
-			require("ibl").setup {
-				char = "▏",
-				show_trailing_blankline_indent = false,
-				show_first_indent_level = true,
-				use_treesitter = true,
-				show_current_context = true,
-				buftype_exclude = { "terminal", "nofile" },
-				filetype_exclude = {
-					"help",
-					"NvimTree",
-				},
-			}
-		end,
+		opts = {
+			char = "▏",
+			show_trailing_blankline_indent = false,
+			show_first_indent_level = true,
+			use_treesitter = true,
+			show_current_context = true,
+			buftype_exclude = { "terminal", "nofile" },
+			filetype_exclude = {
+				"help",
+				"NvimTree",
+			},
+		},
 	},
 }
 
