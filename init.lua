962dafd34f08cfe003d9ba5880dd5bb6b8d13396
me <me@box>
2024-05-20 11:52:00 +0500

    temp rollback

--- init.lua
+++ init.lua
@@ -1,7 +1,11 @@
 require("plugin-loader")
 require("core.additional")
+require('core.theme_picker')
 require("core.settings")
 require("core.autocmds")
-require("core.theme_picker")
 require("core.keymaps")
 
+
+
+-- vim.api.nvim_set_keymap("n", "<Space>t", ":lua require('core.nthemer').PickTheme()<CR>", {noremap = true})
+-- themer.SetThemeFromCache()
