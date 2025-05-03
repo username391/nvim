722bcfc8070468fd8e7c5111acddc7841e864a91
me <me@box>
2025-05-03 18:31:44 +0500

    Перенес конфиг lualine из папки в файл и сменил тему на catpuccin

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,17 @@
+local M = { "nvim-lualine/lualine.nvim" }
+
+M.dependencies = {
+  "nvim-tree/nvim-web-devicons",
+  "catppuccin/nvim"
+}
+
+M.config = function()
+  require("lualine").setup({
+    options = {
+      theme = "catppuccin"
+    }
+  })
+end
+
+return M
+
