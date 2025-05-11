2efd5fbf802ce02d36323d6c2f38b3adbd49ba3f
me <me@box>
2025-05-12 04:57:06 +0500

    исправил неверную идентацию

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -2,9 +2,9 @@ local lspconfig = require("lspconfig")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 local servers = {
-  pyright = require("plugins.lsp.settings.pyright"),
-  gopls = require("plugins.lsp.settings.gopls"),
-  bashls = require("plugins.lsp.settings.bashls"),
+	pyright = require("plugins.lsp.settings.pyright"),
+	gopls = require("plugins.lsp.settings.gopls"),
+	bashls = require("plugins.lsp.settings.bashls"),
 }
 
 for name, config in pairs(servers) do
