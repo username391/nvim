ec50e78a2d1d8d4c9cc8a8ea5841e7e81eef002c
me <me@box>
2025-05-25 07:57:19 +0500

    удалил закомментированный код
    Это был кеймап для того, чтобы открывать nvim changelog. Мне это не
    нужно все равно

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -408,34 +408,3 @@ vim.api.nvim_set_keymap('n', ']c', '<cmd>Gitsigns next_hunk<CR>', { noremap = tr
 vim.api.nvim_set_keymap('n', '[c', '<cmd>Gitsigns prev_hunk<CR>', { noremap = true, silent = true })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
--- {
--- 	"<Space>N",
--- 	desc = "Neovim News",
--- 	function()
--- 		Snacks.win({
--- 			file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
--- 			width = 0.6,
--- 			height = 0.6,
--- 			wo = {
--- 				spell = false,
--- 				wrap = false,
--- 				signcolumn = "yes",
--- 				statuscolumn = " ",
--- 				conceallevel = 3,
--- 			},
--- 		})
--- 	end,
--- }
