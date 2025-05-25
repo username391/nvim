6b38924a59ae4e82c69451b3e27f2c6deddd6ab3
me <me@box>
2025-05-25 07:44:26 +0500

    закомментировал уже неактуальный код

--- lua/core/additional.lua
+++ lua/core/additional.lua
@@ -1,45 +1,45 @@
-local notify = require("notify")
-local nvim_tree_api = require('nvim-tree.api')
+-- local notify = require("notify")
+-- local nvim_tree_api = require('nvim-tree.api')
 
 
-local function getCurrentNode()
-	if nvim_tree_api.tree.is_tree_buf() then
-		local node = nvim_tree_api.tree.get_node_under_cursor()
-		return node
-	else
-		local absolute_path = vim.fn.expand("%:p")
-		local filename = vim.fn.expand("%:t")
-
-		return { name = filename, absolute_path = absolute_path}
-	end
-end
-
-
-function RunCommandOnCurrentFile(command)
-	local node = getCurrentNode()
-	-- notify не будет работать так, как нужно
-	-- если neovim запущен в cmd или powershell
-	local handle = io.popen(command .. " " .. node.absolute_path)
-	local result = ""
-
-	if handle then
-		result = handle:read("*a")
-		handle:close()
-	else
-		result = "ERROR: handle == nil"
-	end
-
-	notify(result, "info", { title = command .. " " .. node.name})
-end
+-- local function getCurrentNode()
+-- 	if nvim_tree_api.tree.is_tree_buf() then
+-- 		local node = nvim_tree_api.tree.get_node_under_cursor()
+-- 		return node
+-- 	else
+-- 		local absolute_path = vim.fn.expand("%:p")
+-- 		local filename = vim.fn.expand("%:t")
+--
+-- 		return { name = filename, absolute_path = absolute_path}
+-- 	end
+-- end
 
 
-function GitAdd()
-	RunCommandOnCurrentFile("git add")
-end
-
-function GitReset()
-	RunCommandOnCurrentFile("git reset")
-end
+-- function RunCommandOnCurrentFile(command)
+-- 	local node = getCurrentNode()
+-- 	-- notify не будет работать так, как нужно
+-- 	-- если neovim запущен в cmd или powershell
+-- 	local handle = io.popen(command .. " " .. node.absolute_path)
+-- 	local result = ""
+--
+-- 	if handle then
+-- 		result = handle:read("*a")
+-- 		handle:close()
+-- 	else
+-- 		result = "ERROR: handle == nil"
+-- 	end
+--
+-- 	-- notify(result, "info", { title = command .. " " .. node.name})
+-- end
+--
+--
+-- function GitAdd()
+-- 	RunCommandOnCurrentFile("git add")
+-- end
+--
+-- function GitReset()
+-- 	RunCommandOnCurrentFile("git reset")
+-- end
 
 -- Это кстати не работает
 function AfterSave()
@@ -56,7 +56,7 @@ function GetShell()
 	if string.sub(os_name, 1, string.len("Windows")) == "Windows" then
 		return "cmd.exe"
 	else
-		return "bash"
+		return "zsh"
 	end
 end
 
