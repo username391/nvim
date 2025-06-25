93ae9a93f7f480f7fb2cc48cb3d081ccefcd6cd8
me <me@box>
2025-06-25 14:14:09 +0500

    fix(lsp): ensure Mason is set up before mason-lspconfig
    - Change opts to config functions for explicit setup order
    - Add dependency relationship to ensure proper loading
    - Use require().setup() calls to guarantee Mason is initialized first

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,57 +1,59 @@
 local plugins = {
 	{
 		"williamboman/mason.nvim",
-		dependencies = {
-			"williamboman/mason-lspconfig.nvim",
-		},
-		opts = {
-			ensure_installed = {
-				-- LSP servers
-				"lua_ls",
-				"pyright",
-				"ruff",
-				"gopls",
-				"bashls",
-				"ts_ls",
-				"jsonls",
-				"cssls",
-				"html",
-				"vimls",
-				"powershell_es",
-				-- Formatters
-				"stylua",
-				"shfmt",
-				"prettier",
-				"djlint",
-				-- Linters
-				"shellcheck",
+		config = function()
+			require("mason").setup({
+				ensure_installed = {
+					-- LSP servers
+					"lua_ls",
+					"pyright",
+					"ruff",
+					"gopls",
+					"bashls",
+					"ts_ls",
+					"jsonls",
+					"cssls",
+					"html",
+					"vimls",
+					"powershell_es",
+					-- Formatters
+					"stylua",
+					"shfmt",
+					"prettier",
+					"djlint",
+					-- Linters
+					-- "shellcheck",
 
-				-- "clangd",
-				-- "intelephense",
-				-- "emmet_ls",
-				-- "omnisharp",
-				-- "yamlls",
-				-- "lemminx",
-				-- "cmake",
-			},
-		},
+					-- "clangd",
+					-- "intelephense",
+					-- "emmet_ls",
+					-- "omnisharp",
+					-- "yamlls",
+					-- "lemminx",
+					-- "cmake",
+				},
+			})
+		end,
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
-		opts = {
-			ensure_installed = {
-				"lua_ls",
-				"pyright",
-				"ruff",
-				"gopls",
-				"bashls",
-				"ts_ls",
-				"jsonls",
-				"cssls",
-				"html",
-			},
-			automatic_installation = true,
-		},
+		dependencies = { "williamboman/mason.nvim" },
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = {
+					"lua_ls",
+					"pyright",
+					"ruff",
+					"gopls",
+					"bashls",
+					"ts_ls",
+					"jsonls",
+					"cssls",
+					"html",
+				},
+				automatic_installation = true,
+			})
+		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
