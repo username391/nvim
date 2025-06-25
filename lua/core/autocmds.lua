1afe1e03f9904f6df9e147676e4f3dc3e972dcef
me <me@box>
2025-06-25 13:10:47 +0500

    style: improve code formatting and consistency in autocmds
    - Convert single quotes to double quotes for consistency
    - Fix indentation to use tabs consistently
    - Remove empty lines and improve readability
    - Clean up autocmd callback structure

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -1,34 +1,33 @@
 -- Запуск разных autocmd
 -- FIXME: Тут во всем нужно разобраться
 
-local ignore_filetypes = { 'NvimTree' }
-local ignore_buftypes = { 'nofile', 'prompt', 'popup' }
+local ignore_filetypes = { "NvimTree" }
+local ignore_buftypes = { "nofile", "prompt", "popup" }
 
-local augroup = vim.api.nvim_create_augroup('FocusDisable', { clear = true })
+local augroup = vim.api.nvim_create_augroup("FocusDisable", { clear = true })
 
-vim.api.nvim_create_autocmd('WinEnter', {
-    group = augroup,
-    callback = function(_)
-        if vim.tbl_contains(ignore_buftypes, vim.bo.buftype)
-        then
-            vim.w.focus_disable = true
-        else
-            vim.w.focus_disable = false
-        end
-    end,
-    desc = 'Disable focus autoresize for BufType',
+vim.api.nvim_create_autocmd("WinEnter", {
+	group = augroup,
+	callback = function(_)
+		if vim.tbl_contains(ignore_buftypes, vim.bo.buftype) then
+			vim.w.focus_disable = true
+		else
+			vim.w.focus_disable = false
+		end
+	end,
+	desc = "Disable focus autoresize for BufType",
 })
 
-vim.api.nvim_create_autocmd('FileType', {
-    group = augroup,
-    callback = function(_)
-        if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
-            vim.w.focus_disable = true
-        else
-            vim.w.focus_disable = false
-        end
-    end,
-    desc = 'Disable focus autoresize for FileType',
+vim.api.nvim_create_autocmd("FileType", {
+	group = augroup,
+	callback = function(_)
+		if vim.tbl_contains(ignore_filetypes, vim.bo.filetype) then
+			vim.w.focus_disable = true
+		else
+			vim.w.focus_disable = false
+		end
+	end,
+	desc = "Disable focus autoresize for FileType",
 })
 
 function IsFile()
@@ -43,23 +42,22 @@ function OpenTreeIfNoFileIsOpened()
 	end
 end
 
-
-vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = function()
-	-- OpenTreeIfNoFileIsOpened()
-end
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+	callback = function()
+		-- OpenTreeIfNoFileIsOpened()
+	end,
 })
 
 vim.api.nvim_create_autocmd("BufReadPre", {
-  pattern = { "*.bat", "*.cmd", "*.lnk" },
-  callback = function()
-    vim.opt.fileencoding = "cp1251"
-  end,
+	pattern = { "*.bat", "*.cmd", "*.lnk" },
+	callback = function()
+		vim.opt.fileencoding = "cp1251"
+	end,
 })
 
-
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "snacks-explorer",
-  callback = function()
-    vim.keymap.set("n", "<C-n>", "<Nop>", { buffer = true })
-  end,
+	pattern = "snacks-explorer",
+	callback = function()
+		vim.keymap.set("n", "<C-n>", "<Nop>", { buffer = true })
+	end,
 })
