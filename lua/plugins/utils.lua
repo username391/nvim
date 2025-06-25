1665fc88d3a827ac4861b5459f08179dad82ca14
me <me@box>
2025-06-25 14:02:35 +0500

    refactor: remove duplicate lazydev configuration from utils.lua
    - Move lazydev configuration consolidation to dedicated lazydev.lua file
    - Eliminate duplicate plugin loading for better performance
    - Clean up utils.lua to focus on utility plugins only
    - Maintains nvim-cmp integration through proper lazydev.lua configuration

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -1,27 +1,5 @@
 return {
-	{
-		"folke/lazydev.nvim",
-		ft = "lua",
-		opts = {
-			library = {
-				{
-					path = "luvit-meta/library",
-					words = { "vim%.uv" }
-				},
-			},
-		},
-	},
-	{ "Bilal2453/luvit-meta", lazy = true },
-	{
-		"hrsh7th/nvim-cmp",
-		opts = function(_, opts)
-			opts.sources = opts.sources or {}
-			table.insert(opts.sources, {
-				name = "lazydev",
-				group_index = 0,
-			})
-		end,
-	},
+	-- lazydev configuration moved to lua/plugins/lazydev.lua
     {
         "nvim-lua/plenary.nvim",
         lazy = true,
