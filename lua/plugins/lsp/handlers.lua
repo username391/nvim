4cf7c545a530a8d0ac4a57b955fb1d1b97fbb637
me <me@box>
2025-06-26 13:31:01 +0500

    refactor

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -14,22 +14,10 @@ local servers = {
 	ruff = require("plugins.lsp.settings.ruff"),
 	gopls = require("plugins.lsp.settings.gopls"),
 	bashls = require("plugins.lsp.settings.bashls"),
-	ts_ls = require("plugins.lsp.settings.ts_ls")
+	ts_ls = require("plugins.lsp.settings.ts_ls"),
 }
 
 for name, config in pairs(servers) do
-  config.capabilities = capabilities
-  lspconfig[name].setup(config)
+	config.capabilities = capabilities
+	lspconfig[name].setup(config)
 end
-
--- lspconfig.pyright.setup({
--- 	filetypes = { "python" }
--- })
---
--- lspconfig.gopls.setup({
--- 	filetypes = { "go", "golang" }
--- })
---
--- lspconfig.bashls.setup({
---   filetypes = { "sh", "bash" },
--- })
