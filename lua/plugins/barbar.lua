f0d3bafe5e5f3e7d8858f7ba57b13e1b421cab25
me <me@box>
2025-12-12 10:22:46 +0500

    меняю формат, теперь все в одной таблице

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,19 +1,44 @@
--- Плагин для вкладок. Я до сих пор не определился
--- нужен ли он мне
-
-local M = {
-	'romgrk/barbar.nvim'
-}
-
-M.enabled = true
-
-M.config = function()
-	require("barbar").setup {
+return {
+	'romgrk/barbar.nvim',
+	enabled = true,
+	opts = {
 		auto_hide = true,
 		animation = true,
-		separator = {left = "▎|", right = ""},
-		--separator = {left = "", right = ""},
-	}
-end
 
-return M
+		icons = {
+			buffer_index = false,
+			buffer_number = false,
+			button = '',
+
+			gitsigns = {
+				added = { enabled = true, icon = '+' },
+				changed = { enabled = true, icon = '~' },
+				deleted = { enabled = true, icon = '-' },
+			},
+
+			filetype = {
+				custom_colors = false,
+				enabled = true,
+			},
+			separator = { left = '▎', right = '' },
+
+			-- If true, add an additional separator at the end of the buffer list
+			separator_at_end = true,
+
+			-- Configure the icons on the bufferline when modified or pinned.
+			-- Supports all the base icon options.
+			modified = { button = '●' },
+			pinned = { button = '', filename = true },
+
+			-- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
+			preset = 'default',
+
+			-- Configure the icons on the bufferline based on the visibility of a buffer.
+			-- Supports all the base icon options, plus `modified` and `pinned`.
+			alternate = { filetype = { enabled = false } },
+			current = { buffer_index = true },
+			-- inactive = { button = '×' },
+			visible = { modified = { buffer_number = false } },
+		},
+	}
+}
