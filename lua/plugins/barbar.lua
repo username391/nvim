0c1d0ea95e4f4c24aaf8392e248c217cf6ab0a6d
me <me@box>
2025-12-12 14:45:01 +0500

    refactor (lsp)

--- lua/plugins/barbar.lua
+++ lua/plugins/barbar.lua
@@ -1,5 +1,5 @@
 return {
-	'romgrk/barbar.nvim',
+	"romgrk/barbar.nvim",
 	enabled = true,
 	opts = {
 		auto_hide = true,
@@ -8,30 +8,30 @@ return {
 		icons = {
 			buffer_index = false,
 			buffer_number = false,
-			button = '',
+			button = "",
 
 			gitsigns = {
-				added = { enabled = true, icon = '+' },
-				changed = { enabled = true, icon = '~' },
-				deleted = { enabled = true, icon = '-' },
+				added = { enabled = true, icon = "+" },
+				changed = { enabled = true, icon = "~" },
+				deleted = { enabled = true, icon = "-" },
 			},
 
 			filetype = {
 				custom_colors = false,
 				enabled = true,
 			},
-			separator = { left = '▎', right = '' },
+			separator = { left = "▎", right = "" },
 
 			-- If true, add an additional separator at the end of the buffer list
 			separator_at_end = true,
 
 			-- Configure the icons on the bufferline when modified or pinned.
 			-- Supports all the base icon options.
-			modified = { button = '●' },
-			pinned = { button = '', filename = true },
+			modified = { button = "●" },
+			pinned = { button = "", filename = true },
 
 			-- Use a preconfigured buffer appearance— can be 'default', 'powerline', or 'slanted'
-			preset = 'default',
+			preset = "default",
 
 			-- Configure the icons on the bufferline based on the visibility of a buffer.
 			-- Supports all the base icon options, plus `modified` and `pinned`.
@@ -40,5 +40,5 @@ return {
 			-- inactive = { button = '×' },
 			visible = { modified = { buffer_number = false } },
 		},
-	}
+	},
 }
