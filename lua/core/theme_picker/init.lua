dbb0237ff9e372e100267b4b2c176d8f34f962f6
me <me@box>
2025-12-12 15:06:37 +0500

    старый код

--- lua/core/theme_picker/init.lua
+++
@@ -1,6 +0,0 @@
-M = require('core.theme_picker.switcher')
-require('core.theme_picker.picker')
-
-
-vim.api.nvim_set_keymap("n", "<Space>t", ":lua PickTheme()<CR>", {noremap = true})
-M.set_theme_from_file()
