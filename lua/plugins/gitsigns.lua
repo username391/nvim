20d1d8e862f87c26ed8d02be03194d0c8d3716f6
me <me@box>
2025-12-12 15:04:19 +0500

    refactor

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,31 +1,28 @@
--- Отображает изменения в текущем репозитории
-local M = {
-	"lewis6991/gitsigns.nvim"
+return {
+	"lewis6991/gitsigns.nvim",
+	enabled = true,
+	event = { "BufReadPost", "BufNewFile" },
+
+	config = function()
+		local gitsigns = require("gitsigns")
+
+		gitsigns.setup({
+			signs = {
+				add = { text = "+▎" },
+				change = { text = "~▎" },
+				delete = { text = "" },
+				topdelete = { text = "" },
+				changedelete = { text = "▎" },
+			},
+			attach_to_untracked = false,
+			preview_config = {
+				-- Options passed to nvim_open_win
+				border = "rounded",
+				style = "minimal",
+				relative = "cursor",
+				row = 0,
+				col = 1,
+			},
+		})
+	end,
 }
-M.enabled = true
-M.event = { "BufReadPost", "BufNewFile" }
-
-M.config = function()
-	local gitsigns = require("gitsigns")
-
-	gitsigns.setup {
-		signs = {
-			add = { text = "+▎", },
-			change = { text = "~▎", },
-			delete = { text = "", },
-			topdelete = { text = "", },
-			changedelete = { text = "▎", },
-		},
-		attach_to_untracked = false,
-		preview_config = {
-			-- Options passed to nvim_open_win
-			border = "rounded",
-			style = "minimal",
-			relative = "cursor",
-			row = 0,
-			col = 1,
-		},
-	}
-end
-
-return M
