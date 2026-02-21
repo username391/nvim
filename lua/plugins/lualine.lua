5cde408ac8cfc3157484733b02554202d677d38e
username391 <username391@box>
2026-02-21 18:09:22 +0500

    changed some modules and changed theme

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -8,8 +8,12 @@ M.dependencies = {
 M.config = function()
 	require("lualine").setup({
 		options = {
-			-- theme = "catppuccin"
-			theme = "tokyonight",
+			theme = "catppuccin",
+			-- theme = "tokyonight",
+			disabled_filetypes = {
+				statusline = { "snacks*", "snacks_picker", "snacks_dashboard" },
+				winbar = { "snacks_picker_list" },
+			},
 		},
 	})
 end
